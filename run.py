a18484226d863968a4dd81bd9aba6fe53e24a92a
Erx35 Kannikka <ergokannikka@gmail.com>
2024-05-20 19:11:04 +0000

    add loop for data aqusition

--- run.py
+++ run.py
@@ -18,14 +18,17 @@ def get_ales_data():
     """
     Get sales data input from the user
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here: ")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+        
+        if validate_data(sales_data):
+            break
 
 
 def validate_data(values):
@@ -42,7 +45,8 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
 
-
+    return True
 
 get_ales_data()
